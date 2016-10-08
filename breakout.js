e17da6c31a72737e81dc752db594171dd878f397
De-Cay <dushik@live.com>
2016-10-08 11:51:07 +0530

    ball hits the paddle

--- breakout.js
+++ breakout.js
@@ -6,15 +6,18 @@ var gameIntialize = function () {
     var canvas = document.getElementById("canvas-box"),
         ctx = canvas.getContext("2d"),
         x = canvas.width / 2,
-        y = canvas.height - 10,
+        y = canvas.height / 2,
         dx = 2,
         dy = -2,
         ballRadius = 10,
         paddleHeight = 10,
         paddleWidth = 75,
         paddleX = (canvas.width - paddleWidth) / 2,
+        paddleY = canvas.height - paddleHeight - 15,
         rightArrowPressed = false,
         leftArrowPressed = false;
+
+    localStorage.setItem('alerted','no');
 
     function keyDownHandler(e) {
         if (e.keyCode === 39) {
@@ -43,7 +46,7 @@ var gameIntialize = function () {
 
     function drawPaddle() {
         ctx.beginPath();
-        ctx.rect(paddleX, canvas.height - paddleHeight - 20, paddleWidth, paddleHeight);
+        ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
         ctx.fillStyle = "#0095DD";
         ctx.fill();
         ctx.closePath();
@@ -53,21 +56,39 @@ var gameIntialize = function () {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
         drawBall();
         drawPaddle();
-        if ((x + dx > canvas.width - ballRadius) || (x + dx < ballRadius)) {
+        if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
             dx = -dx;
         }
-
-        if ((y + dy > canvas.height - ballRadius) || (y + dy < ballRadius)) {
-            dy = -dy;
+        if(y + dy < ballRadius) {
+            dy = -(dy + 0.5);
+        }
+        else if(y + dy >= paddleY) {
+            if(x > paddleX && x < paddleX + paddleWidth) {
+                dy = -(dy + 1);
+            }
+            else {
+                gameOver();
+            }
         }
 
-        if (rightArrowPressed && paddleX < canvas.width - paddleWidth) {
+        if(rightArrowPressed && paddleX < canvas.width - paddleWidth) {
             paddleX += 7;
-        } else if (leftArrowPressed && paddleX > 0) {
+        }
+        else if(leftArrowPressed && paddleX > 0) {
             paddleX -= 7;
         }
         x += dx;
         y += dy;
     }
-    setInterval(moveBallAndPaddle, 5);
+
+    setInterval(moveBallAndPaddle, 8);
+};
+
+var gameOver = function(){
+    var alerted = localStorage.getItem('alerted') || '';
+    if (alerted != 'yes') {
+        alert("Game Over");
+        localStorage.setItem('alerted','yes');
+    }
+    document.location.reload();
 };
