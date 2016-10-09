d1e91901a6e5b39f7d6a5bd230e59a393920e83d
De-Cay <dushik@live.com>
2016-10-09 11:25:14 +0530

    bricks added

--- breakout.js
+++ breakout.js
@@ -15,15 +15,33 @@ var gameIntialize = function () {
         paddleX = (canvas.width - paddleWidth) / 2,
         paddleY = canvas.height - paddleHeight - 15,
         rightArrowPressed = false,
-        leftArrowPressed = false;
+        leftArrowPressed = false,
+        spaceKeyPressed = false,
+        brickRowCount = 4,
+        brickColumnCount = 7,
+        brickWidth = 50,
+        brickHeight = 15,
+        brickPadding = 10,
+        brickOffsetTop = 30,
+        brickOffsetLeft = 30;
 
     localStorage.setItem('alerted','no');
+
+    var bricks = [];
+    for(var ii = 0; ii < brickRowCount; ii++) {
+        bricks[ii] = [];
+        for(var jj = 0; jj < brickColumnCount ; jj++) {
+            bricks[ii][jj] = { x: 0, y: 0 };
+        }
+    }
 
     function keyDownHandler(e) {
         if (e.keyCode === 39) {
             rightArrowPressed = true;
         } else if (e.keyCode === 37) {
             leftArrowPressed = true;
+        } else if (e.keyCode === 32) {
+            spaceKeyPressed = true;
         }
     }
     function keyUpHandler(e) {
@@ -39,7 +57,7 @@ var gameIntialize = function () {
     function drawBall() {
         ctx.beginPath();
         ctx.arc(x, y, ballRadius, 0, Math.PI * 2);
-        ctx.fillStyle = "#0095DD";
+        ctx.fillStyle = "#000000";
         ctx.fill();
         ctx.closePath();
     }
@@ -47,13 +65,30 @@ var gameIntialize = function () {
     function drawPaddle() {
         ctx.beginPath();
         ctx.rect(paddleX, paddleY, paddleWidth, paddleHeight);
-        ctx.fillStyle = "#0095DD";
+        ctx.fillStyle = "#551A8B";
         ctx.fill();
         ctx.closePath();
     }
 
+    function drawBricks() {
+        for(var ii = 0; ii < brickRowCount; ii++) {
+            var brickY = (ii *(brickHeight + brickPadding)) + brickOffsetTop;
+            for(var jj = 0; jj < brickColumnCount  ; jj++) {
+                bricks[ii][jj].y = brickY;
+                var brickX = (jj * (brickWidth + brickPadding)) + brickOffsetLeft;
+                bricks[ii][jj].x = brickX;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = brickColor(ii);
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+
     function moveBallAndPaddle() {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        drawBricks();
         drawBall();
         drawPaddle();
         if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
@@ -71,21 +106,45 @@ var gameIntialize = function () {
             }
         }
 
-        if(rightArrowPressed && paddleX < canvas.width - paddleWidth) {
-            paddleX += 7;
+        if(spaceKeyPressed){
+            if(rightArrowPressed && paddleX < canvas.width - paddleWidth) {
+                paddleX += 7;
+            }
+            else if(leftArrowPressed && paddleX > 0) {
+                paddleX -= 7;
+            }
+            x += dx;
+            y += dy;
         }
-        else if(leftArrowPressed && paddleX > 0) {
-            paddleX -= 7;
-        }
-        x += dx;
-        y += dy;
+
     }
 
     setInterval(moveBallAndPaddle, 8);
 };
 
+var brickColor = function(rowNumber){
+    var color = "red";
+    switch (rowNumber) {
+        case 0:
+            color = "red";
+            break;
+        case 1:
+            color = "orange";
+            break;
+        case 2:
+            color = "yellow";
+            break;
+        case 3:
+            color = "green";
+            break;
+        default:
+            color = "00FFFF";
+    }
+    return color;
+};
+
 var gameOver = function(){
-    var alerted = localStorage.getItem('alerted') || '';
+    var alerted = localStorage.getItem('alerted');
     if (alerted != 'yes') {
         alert("Game Over");
         localStorage.setItem('alerted','yes');
